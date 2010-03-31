b91b35ccd3c96bc67b514a6f26c08aa7eb1bd0d9
Paco Guzman <fjguzman@aspgems.com>
2010-03-31 18:22:55 +0200

    redhillsonrails

--- db/migrate/001_create_activities_table.rb
+++ db/migrate/001_create_activities_table.rb
@@ -1,10 +1,9 @@
 class CreateActivitiesTable < ActiveRecord::Migration
   def self.up
     create_table :activities, :force => true do |t|
-      t.references :author
-
+      t.integer    :author_id, :references => :users
       t.string     :actions
-      t.integer    :object_id
+      t.integer    :object_id, :references => nil
       t.string     :object_type, :limit => 15
       t.text       :object_attributes
       t.datetime   :created_at, :null => false
